edf7e47f393ca64304d52511912a51924288f534
CommonLoon102 <CommonLoon102@users.noreply.github.com>
2020-01-25 15:37:01 +0100

    remove server from the list when it quits

---
+++ Supervisor/Packets/RemoveServer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Supervisor
+{
+    class RemoveServer : PacketData
+    {
+        public RemoveServer()
+        {
+            IsStarted = false;
+        }
+    }
+}
