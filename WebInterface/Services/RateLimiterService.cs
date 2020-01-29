621127757648ebc9bdf7c7b8d482d0ad52544e69
CommonLoon102 <CommonLoon102@users.noreply.github.com>
2020-01-29 15:50:16 +0100

    hash the IPs

--- WebInterface/Services/RateLimiterService.cs
+++ WebInterface/Services/RateLimiterService.cs
@@ -3,24 +3,27 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace WebInterface.Services
 {
     public class RateLimiterService : IRateLimiterService
     {
-        private static readonly ConcurrentDictionary<IPAddress, List<DateTime>> requestHistory = new ConcurrentDictionary<IPAddress, List<DateTime>>();
+        private static readonly ConcurrentDictionary<string, List<DateTime>> requestHistory = new ConcurrentDictionary<string, List<DateTime>>();
 
         public bool IsRequestAllowed(IPAddress remoteIpAddress)
         {
-            if (requestHistory.TryGetValue(remoteIpAddress, out var list))
+            string hash = GetHash(remoteIpAddress);
+            if (requestHistory.TryGetValue(hash, out var list))
             {
                  bool isLimited = list.Count(e => DateTime.UtcNow - e < TimeSpan.FromHours(1)) >= 5;
                 if (isLimited)
                     return false;
             }
 
-            requestHistory.AddOrUpdate(remoteIpAddress,
+            requestHistory.AddOrUpdate(hash,
                 (ip) =>
                 {
                     var list = new List<DateTime>();
@@ -34,6 +37,26 @@ namespace WebInterface.Services
                 });
 
             return true;
+        }
+
+        private string GetHash(IPAddress remoteIpAddress)
+        {
+            const string pepper = "Somewhere, over the rainbow, way up this pepper.";
+            var data = Encoding.ASCII.GetBytes(remoteIpAddress.ToString() + pepper);
+            string hash;
+            using (SHA512 shaM = new SHA512Managed())
+            {
+                byte[] bytes = shaM.ComputeHash(data);
+                for (int i = 0; i < 1000; i++)
+                {
+                    bytes = shaM.ComputeHash(bytes);
+                }
+
+                bytes = bytes.Take(bytes.Length / 4).ToArray();
+                hash = Encoding.ASCII.GetString(bytes);
+            }
+
+            return hash;
         }
     }
 }
