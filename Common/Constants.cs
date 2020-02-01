0c9f5eb194ce7ceb7f6557585b5ae1d76cbd6855
CommonLoon102 <CommonLoon102@users.noreply.github.com>
2020-02-01 23:43:35 +0100

    fix linux paths

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -1,6 +1,7 @@
 ﻿using Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Common
@@ -9,12 +10,17 @@ namespace Common
     {
         public const long FileSizeLimit = 1024 * 1024;
 
+        private const string workingDirectory = "WD";
+        private static readonly string iraArt = Path.Combine(workingDirectory, "IRA");
+        private static readonly string iraRff = Path.Combine(workingDirectory, "BLOOD.RFF");
+        private static readonly string iraSound = Path.Combine(workingDirectory, "SOUNDS.RFF");
+
         public static readonly IReadOnlyDictionary<string, Mod> SupportedMods = new Dictionary<string, Mod>()
         {
             { "BLOOD", new Mod("BLOOD", "Blood", "") },
             { "CRYPTIC", new Mod("CRYPTIC", "Cryptic Passage", "-ini CRYPTIC.INI") },
             { "DW", new Mod("DW", "Death Wish", "-ini dw.ini") },
-            { "TWOIRA", new Mod("TWOIRA", "The Way of Ira", @"-game_dir IRA -ini ira.ini -art WD\IRA -rff WD\BLOOD.RFF -snd WD\SOUNDS.RFF") },
+            { "TWOIRA", new Mod("TWOIRA", "The Way of Ira", $"-game_dir IRA -ini ira.ini -art {iraArt} -rff {iraRff} -snd {iraSound}") },
         };
     }
 }
